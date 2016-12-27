fb5c2cd03ff3c62058d215c97c9e68bda55ce1a7
David G Chaves <davidgchaves@gmail.com>
2016-12-27 18:32:52 +0100

    Fix test

--- src/server/test/models.js
+++ src/server/test/models.js
@@ -1,33 +1,29 @@
-'use strict';
+/* eslint-env node, mocha */
+'use strict'
 
-const assert = require('chai').assert;
-const Sequelize = require('sequelize');
-const db = require('../models/db');
-const Todo = require('../models/todo');
+const assert = require('chai').assert
+const db = require('../models/db')
+const Todo = require('../models/todo')
 
-// reset the test database
-before(function(done) {
-  db.drop().then(function() {
-    return db.sync({ force: true });
-  }).then(function() {
-    done();
-  }).catch(function(err) {
-    console.log(err);
-  });
-});
+const resetTestDB = done => {
+  db
+    .drop()
+    .then(() => db.sync({ force: true }))
+    .then(() => done())
+    .catch((err) => console.log(err))
+}
 
-describe('The Todo Model', function() {
+before(resetTestDB)
 
-  it('should accept text for the TODO item', function() {
-    let todoText = 'this is a super todo';
-    let completed = false;
-    return Todo.create({
-      title: todoText,
-      completed: completed
-    }).then(function(todo) {
-      assert.equal(todo.title, todoText, 'Saved text should match');
-      assert.equal(todo.completed, false, 'Saved text should match');
-    });
-  });
-
-});
+describe('The Todo Model', () => {
+  it('accepts text for the TODO item', () => {
+    const title = 'this is a super todo'
+    const completed = false
+    return Todo
+      .create({ title, completed })
+      .then(todo => {
+        assert.equal(todo.title, title, 'Todo text does not match')
+        assert.equal(todo.completed, completed, 'Completed todo status does not match')
+      })
+  })
+})
